6f14683a7a4534121dfc37d77c0d3d5f750aa46d
David <dhsists@gmail.com>
2014-12-03 23:45:05 -0500

    register files

---
+++ public/register.php
@@ -0,0 +1,51 @@
+<?php
+
+    // configuration
+    require("../includes/config.php");
+
+    // if user reached page via GET (as by clicking a link or via redirect)
+    if ($_SERVER["REQUEST_METHOD"] == "GET")
+    {
+        // else render form
+        render("register_form.php", ["title" => "Register"]);
+    }
+
+    // else if user reached page via POST (as by submitting a form via POST)
+    else if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        // error checks for all cases
+        if (empty($_POST["username"]))
+        apologize("Please provide username"); 
+        
+        else if (empty($_POST["password"]))
+        apologize("Please provide a password");
+        
+        else if (empty($_POST["confirmation"]))
+        apologize("Please provide a confirmation password");
+        
+        else if ($_POST["password"] != $_POST["confirmation"])
+        apologize("Your passwords do not match");
+        
+        if (query("INSERT INTO users (username, hash, cash) VALUES(?, ?, 10000.00)", $_POST["username"], crypt($_POST["password"])) === false)
+        apologize("This username is already being used");
+        
+        else
+        {
+            $rows = query("SELECT LAST_INSERT_ID() AS id");
+            $id = $rows[0]["id"];
+            
+            $_SESSION["id"] = $id;
+            redirect("index.php");
+        }
+        
+        
+    }
+    
+    else
+    {
+        render("register_form.php", ["title" => "Register"]);
+    }
+    
+   
+
+?>
