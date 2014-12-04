b1f02d63c02494c92ba4725aa8b7dade9266dd58
Karl Otness <karl@karlotness.com>
2014-12-04 00:16:01 -0500

    Configure the project to use the musicmates database.

--- includes/constants.php
+++ includes/constants.php
@@ -10,7 +10,7 @@
      */
 
     // your database's name
-    define("DATABASE", "pset7");
+    define("DATABASE", "musicmates");
 
     // your database's password
     define("PASSWORD", "crimson");
