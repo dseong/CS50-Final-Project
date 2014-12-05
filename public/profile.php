59f62d9822a2935b77468703ce6098fad5c4e6ff
Karl Otness <karl@karlotness.com>
2014-12-05 02:12:45 -0500

    Add lists of groups that a user owns and is a member of to profile.

--- public/profile.php
+++ public/profile.php
@@ -10,8 +10,12 @@
     $user = query("SELECT * FROM users WHERE id=?", $userid);
     // Get user's instrument details
     $instruments = query("SELECT * FROM instruments WHERE userid=?", $userid);
+    // Get user's group memberships
+    $memberships = query("SELECT G.name, G.id as gid, G.genre, GI.instrument FROM groupinsts AS GI LEFT JOIN groups AS G ON G.id = GI.groupid WHERE GI.userid = ?", $userid);
+    // Get groups owned by user (these could be different things)
+    $owned = query("SELECT G.id, G.name, (SELECT COUNT(id) FROM groupinsts WHERE groupid=G.id) as slotcnt, (SELECT COUNT(id) FROM groupinsts WHERE groupid=G.id AND userid IS NOT NULL) as fullslot FROM groups AS G WHERE ownerid=?", $userid);
     
-    if($user === false || $instruments === false)
+    if($user === false || $instruments === false || $memberships === false || $owned === false)
     {
         // Database error
         apologize("An error occurred while fetching your account details.");
@@ -21,6 +25,8 @@
     $data["name"]  = $user[0]["name"];
     $data["email"] = $user[0]["email"];
     $data["instruments"] = $instruments;
+    $data["memberships"] = $memberships;
+    $data["owned"] = $owned;
     $data["title"] = "Profile";
     
     // Render the template for the user
--- templates/profile_view.php
+++ templates/profile_view.php
@@ -5,9 +5,47 @@
     <a href="/edit_profile.php">Edit Profile</a>
 </div>
 <div class="datacont">
-    <p class="datahead">Your Instruments</p>
-    <?php foreach($instruments as $inst): ?>
-        <p><?= htmlspecialchars($inst["instrument"]); ?> <a href="<?= "/delinst.php?id=" . $inst["id"]?>">Delete</a></p>
-    <?php endforeach; ?>
+    <?php if(!empty($instruments)): ?>
+        <p class="datahead">Your Instruments</p>
+        <?php foreach($instruments as $inst): ?>
+            <p><?= htmlspecialchars($inst["instrument"]); ?> <a href="<?= "/delinst.php?id=" . $inst["id"]?>">Delete</a></p>
+        <?php endforeach; ?>
+    <?php endif; ?>
     <a href="/addinstrument.php">Add Instrument</a>
+    
+<?php if(!empty($memberships)): ?>
+    <div class="datacont">
+        <table class="table">
+            <thead>
+                <td>Group Name</td>
+                <td>Group Instrument</td>
+            </thead>
+            <p class="datahead">Your Group Memberships</p>
+            <?php foreach($memberships as $memb): ?>
+                <tr>
+                    <td><a href="<?= "/group.php?id=".$memb["gid"] ?>"><?= htmlspecialchars($memb["name"]) ?></td>
+                    <td><?= htmlspecialchars($memb["instrument"]) ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </div>
+<?php endif; ?>
+
+<?php if(!empty($owned)): ?>
+    <div class="datacont">
+        <table class="table">
+            <thead>
+                <td>Group Name</td>
+                <td>Members</td>
+            </thead>
+            <p class="datahead">Groups You Own</p>
+            <?php foreach($owned as $own): ?>
+                <tr>
+                    <td><a href="<?= "/group.php?id=".$own["id"] ?>"><?= htmlspecialchars($own["name"]) ?></td>
+                    <td><?= htmlspecialchars($own["fullslot"]."/".$own["slotcnt"]) ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </div>
+<?php endif; ?>
 </div>
