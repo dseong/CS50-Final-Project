949ddb61a85b4d00ca485ecb76574871aa213e39
Karl Otness <karl@karlotness.com>
2014-12-05 22:45:05 -0500

    Add home page to site.
    This page will be displayed to users who are not logged in.

---
+++ templates/home.php
@@ -0,0 +1,10 @@
+<div class="content-left">
+    <div class="jumbotron" id="home-info">
+        <h2>Looking for a chamber group?</h2>
+        <p>MusicMates can help you find other musicians.<br/>
+        Form a group to play your kind of music, at your level.</p>
+        <a href="/register.php" class="btn btn-primary">Register Now</a>
+        <a href="/login.php" class="btn btn-default">Log In</a>
+        <div id="img-attr"><a href="https://www.flickr.com/photos/pquero/7635974162/"><span class="glyphicon glyphicon-camera"></span> Flickr: pquero</a></div>
+    </div>
+</div>
