792f5dc51790058c97e0d1181db59553d0ce462b
Karl Otness <karl@karlotness.com>
2014-12-06 21:06:39 -0500

    Modify search field stylings.

--- templates/search_form.php
+++ templates/search_form.php
@@ -1,47 +1,53 @@
 <div class="row">
     <div class="col-md-3">
-        <form action="search.php" method="get">
-            <fieldset>
-                <div class="form-group">
-                    <input class="form-control" <?php if(!empty($username))echo("value=\"".$username."\""); ?> name="username" placeholder="Owner's Username" type="text"/>
-                </div>
-                <div class="form-group">
-                    <input class="form-control" <?php if(!empty($members))echo("value=\"".$members."\""); ?> name="members" placeholder="Group Members" type="text"/>
-                </div>
-                <div class="form-group">
-                    <select class="form-control" name="instrument">
-                        <option value="--none--">Open Slot for Instrument</option>
-                        <?php foreach($instruments as $instrument): ?>
-                            <option <?php if(!empty($selinstrument)&&$selinstrument===$instrument["instrument"])echo("selected"); ?> value="<?= htmlspecialchars($instrument["instrument"]) ?>">
-                                <?= htmlspecialchars($instrument["instrument"]) ?>
-                            </option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <select class="form-control" name="skill">
-                        <option value="--none--">Group Skill Level</option>
-                        <?php foreach($skills as $skill): ?>
-                            <option <?php if(!empty($selskill)&&$selskill===$skill["id"])echo("selected"); ?> value="<?= htmlspecialchars($skill["id"]) ?>">
-                                <?= htmlspecialchars($skill["description"]) ?>
-                            </option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <select class="form-control" name="genre">
-                        <option value="--none--">Group Genre</option>
-                        <?php foreach($genres as $genre): ?>
-                            <option <?php if(!empty($selgenre)&&$selgenre===$genre["name"])echo("selected"); ?> value="<?= htmlspecialchars($genre["name"]) ?>">
-                                <?= htmlspecialchars($genre["name"]) ?>
-                            </option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-            </fieldset>
-            <input class="btn btn-default" type="hidden" name="search" value="yes"/>
-            <input class="btn btn-default" type="submit" value="Search"/>
-        </form>
+        <div id="searchform">
+            <form action="search.php" method="get">
+                <fieldset>
+                    <div class="form-group">
+                        <input class="form-control " <?php if(!empty($username))echo("value=\"".$username."\""); ?> name="username" placeholder="Owner's Username" type="text"/>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control " <?php if(!empty($members))echo("value=\"".$members."\""); ?> name="members" placeholder="Group Members" type="text"/>
+                    </div>
+                    <div class="form-group">
+                        <select class="form-control " name="instrument">
+                            <option value="--none--">Open Slot for Instrument</option>
+                            <?php foreach($instruments as $instrument): ?>
+                                <option <?php if(!empty($selinstrument)&&$selinstrument===$instrument["instrument"])echo("selected"); ?> value="<?= htmlspecialchars($instrument["instrument"]) ?>">
+                                    <?= htmlspecialchars($instrument["instrument"]) ?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="form-group ">
+                        <select class="form-control " name="skill">
+                            <option value="--none--">Group Skill Level</option>
+                            <?php foreach($skills as $skill): ?>
+                                <option <?php if(!empty($selskill)&&$selskill===$skill["id"])echo("selected"); ?> value="<?= htmlspecialchars($skill["id"]) ?>">
+                                    <?= htmlspecialchars($skill["description"]) ?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <select class="form-control" name="genre">
+                            <option value="--none--">Group Genre</option>
+                            <?php foreach($genres as $genre): ?>
+                                <option <?php if(!empty($selgenre)&&$selgenre===$genre["name"])echo("selected"); ?> value="<?= htmlspecialchars($genre["name"]) ?>">
+                                    <?= htmlspecialchars($genre["name"]) ?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <input class="btn btn-default form-control" type="hidden" name="search" value="yes"/>
+                    </div>
+                    <div class="form-group">
+                        <input class="btn btn-default form-control" type="submit" value="Search"/>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
     </div>
     <div class="col-md-9">
         <?php if($search): ?>
